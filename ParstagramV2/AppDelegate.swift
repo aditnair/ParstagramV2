decd89c266200d9128e569bee503030343e38214
aditnair <adityanair@ufl.edu>
2021-10-18 01:49:40 -0400

    Complete

--- ParstagramV2/AppDelegate.swift
+++ ParstagramV2/AppDelegate.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            
+                $0.applicationId = "E7soJu0BqhJqSlsLqYSgms2a8Nf9vkR6zLQJnE96" // <- UPDATE
+            
+                $0.clientKey = "u4LlL1db7pmhGqHFoad8aCs6Xn3VbwdOhCvdVilm" // <- UPDATE
+            
+                $0.server = "https://parseapi.back4app.com"
+            
+        }
+        
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ ParstagramV2/LoginViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LoginViewController.swift
+//  ParstagramV2
+//
+//  Created by Aditya Nair on 10/17/21.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func OnLogIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { ( user , error ) in
+            
+            if user != nil {
+                
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                
+            } else {
+                
+                print("Error: \(error?.localizedDescription)")
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        let user = PFUser()
+        
+        user.username = usernameField.text
+        
+        user.password = passwordField.text
+        
+        user.signUpInBackground { ( success, error) in
+            if success {
+                
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                
+            } else {
+                
+                print("Error \(error?.localizedDescription)")
+                
+            }
+            
+        }
+        
+    }
+
+}
